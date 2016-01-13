681bd038a1db88f9cd79c8a26e808d5623dbb06f
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2016-01-13 22:31:33 +0800

    TFB: add postgresql

--- tfb-go/db-files/setup.sh
+++ tfb-go/db-files/setup.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 
 cat mysql-create.sql | mysql -u root
+
+sudo -u postgres psql template1 < postgres-create-db.sql
+sudo -u benchmarkdbuser psql hello_world < postgres-create.sql
--- tfb-go/db-files/sudo-setup.sh
+++ tfb-go/db-files/sudo-setup.sh
@@ -5,11 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y install sysstat
 apt-get -y install mysql-server
+apt-get -y install postgresql
 
-#sed -i 's|\[mysqld\]|\[mysqld\]\
-#lower_case_table_names = 1\
-#character-set-server=utf8\
-#collation-server=utf8_general_ci|g' /etc/mysql/my.cnf
-
-#sed -i 's|bind-address.*=.*127.0.0.1|bind-address = 0.0.0.0|g' /etc/mysql/my.cnf
-
+sudo useradd benchmarkdbuser -p benchmarkdbpass
