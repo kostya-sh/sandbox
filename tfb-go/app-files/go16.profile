178e5935ab20e3584433d93b56b7fc2241ce0bb4
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2016-01-14 22:47:29 +0800

    TFB: add go 1.6beta2, upgrade to go 1.5.3, some results
    Unfortunately my SPOT instance stopped, I'll add more results later.

---
+++ tfb-go/app-files/go16.profile
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export GOPATH=$HOME
+export GOROOT=$HOME/go1.6
+export PATH=$HOME/bin:$GOROOT/bin:$PATH
--- tfb-go/app-files/setup.sh
+++ tfb-go/app-files/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-wget -c https://storage.googleapis.com/golang/go1.5.2.linux-amd64.tar.gz
-tar xf go1.5.2.linux-amd64.tar.gz
+wget -c https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
+tar xf go1.5.3.linux-amd64.tar.gz
 mv go go1.5
+
+wget -c https://storage.googleapis.com/golang/go1.6beta2.linux-amd64.tar.gz
+tar xf go1.6beta2.linux-amd64.tar.gz
+mv go go1.6
 
 mkdir bin
