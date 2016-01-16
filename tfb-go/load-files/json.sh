975f0678d16934c87f5253c3e6dc7bc2682fcacf
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2016-01-16 15:10:25 +0800

    TFB: add 256 concurrency level

--- tfb-go/load-files/json.sh
+++ tfb-go/load-files/json.sh
@@ -3,6 +3,6 @@
 URL="http://${APPHOST:-localhost}:8080/json"
 
 echo $URL
-for i in 1 2 4 8 16 32 64 128 ; do
+for i in 1 2 4 8 16 32 64 128 256 ; do
     ./wrk -t$i -c$i -d15s "$URL" | awk -f extract-wrk.awk
 done
