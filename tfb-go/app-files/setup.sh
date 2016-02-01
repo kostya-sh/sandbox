bff55c15533fc10278c954ea0445e8c7a933f2ac
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2016-02-01 23:59:51 +0800

    TFB: minor improvements and more results

--- tfb-go/app-files/setup.sh
+++ tfb-go/app-files/setup.sh
@@ -4,8 +4,8 @@ wget -c https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
 tar xf go1.5.3.linux-amd64.tar.gz
 mv go go1.5
 
-wget -c https://storage.googleapis.com/golang/go1.6beta2.linux-amd64.tar.gz
-tar xf go1.6beta2.linux-amd64.tar.gz
+wget -c https://storage.googleapis.com/golang/go1.6rc1.linux-amd64.tar.gz
+tar xf go1.6rc1.linux-amd64.tar.gz
 mv go go1.6
 
 mkdir bin
--- tfb-go/load-files/json_profile.sh
+++ tfb-go/load-files/json_profile.sh
@@ -7,11 +7,13 @@ TRACE=$3
 
 echo $URL
 for i in $THREADS ; do
-    curl "$URL/profile/start?f=${NAME}_$i"
+    curl "$URL/profile/start?f=${NAME}_$i$TRACE"
     if [ "$TRACE" == "-trace" ] ; then
         curl "$URL/trace/start?f=${NAME}_$i"
     fi
+    sleep 1s
     ./wrk -t$i -c$i -d15s "$URL/json" | awk -f extract-wrk.awk
+    sleep 1s
     curl "$URL/profile/stop"
     if [ "$TRACE" == "-trace" ] ; then
         curl "$URL/trace/stop"
