5be4109f94bc71d547bf516cfa54ad3d27b4795c
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2016-02-24 08:39:34 +0800

    update go-guru.el

--- emacs/go-guru.el
+++ emacs/go-guru.el
@@ -11,19 +11,21 @@
 ;;; Commentary:
 
 ;; To install the Go guru, run:
+;;
 ;; $ go get golang.org/x/tools/cmd/guru
 ;;
 ;; Load this file into Emacs and set go-guru-scope to your
-;; configuration.  Then, find a file of Go source code, enable
-;; go-guru-mode, select an expression of interest, and press `C-c C-o d'
-;; (for "describe") or run one of the other go-guru-xxx commands.
+;; configuration.  Then, find a file of Go source code,
+;; select an expression of interest, and press `C-c C-o d' (for "describe")
+;; or run one of the other go-guru-xxx commands.
 
 ;;; Code:
 
 (require 'compile)
 (require 'go-mode)
+(require 'json)
 (require 'simple)
-(require 'cl-lib)
+(require 'cl)
 
 (defgroup go-guru nil
   "Options specific to the Go guru."
@@ -44,20 +46,19 @@
   "History of values supplied to `go-guru-set-scope'.")
 
 ;; Extend go-mode-map.
-(let ((m go-mode-map))
-  (define-key m (kbd "C-c C-o t") #'go-guru-describe) ; t for type
-  (define-key m (kbd "C-c C-o f") #'go-guru-freevars)
-  (define-key m (kbd "C-c C-o g") #'go-guru-callgraph)
-  (define-key m (kbd "C-c C-o i") #'go-guru-implements)
-  (define-key m (kbd "C-c C-o c") #'go-guru-peers)  ; c for channel
-  (define-key m (kbd "C-c C-o r") #'go-guru-referrers)
-  (define-key m (kbd "C-c C-o d") #'go-guru-definition)
-  (define-key m (kbd "C-c C-o p") #'go-guru-pointsto)
-  (define-key m (kbd "C-c C-o s") #'go-guru-callstack)
-  (define-key m (kbd "C-c C-o <") #'go-guru-callers)
-  (define-key m (kbd "C-c C-o >") #'go-guru-callees)
-  (define-key m (kbd "<f5>") #'go-guru-describe)
-  (define-key m (kbd "<f6>") #'go-guru-referrers))
+(let ((m (define-prefix-command 'go-guru-map)))
+  (define-key m "d" #'go-guru-describe)
+  (define-key m "f" #'go-guru-freevars)
+  (define-key m "i" #'go-guru-implements)
+  (define-key m "c" #'go-guru-peers)  ; c for channel
+  (define-key m "r" #'go-guru-referrers)
+  (define-key m "j" #'go-guru-definition) ; j for jump
+  (define-key m "p" #'go-guru-pointsto)
+  (define-key m "s" #'go-guru-callstack) ; s for stack
+  (define-key m "<" #'go-guru-callers)
+  (define-key m ">" #'go-guru-callees))
+
+(define-key go-mode-map (kbd "C-c C-o") #'go-guru-map)
 
 ;; TODO(dominikh): Rethink set-scope some. Setting it to a file is
 ;; painful because it doesn't use find-file, and variables/~ aren't
@@ -73,10 +74,10 @@ previous scope.
 
 The scope specifies a set of arguments, separated by spaces.
 It may be:
-1) a set of packages whose main() functions will be analyzed.
+1) a set of packages whose main functions will be analyzed.
 2) a list of *.go filenames; they will treated like as a single
    package (see #3).
-3) a single package whose main() function and/or Test* functions
+3) a single package whose main function and/or Test* functions
    will be analyzed.
 
 In the common case, this is similar to the argument(s) you would
@@ -92,17 +93,20 @@ specify to 'go build'."
     (setq go-guru-scope scope)))
 
 (defun go-guru--run (mode &optional need-scope)
-  "Run the Go guru in the specified MODE, passing it the
+  "Run the Go guru in the specified MODE, passing it the selected
+region of the current buffer.  If NEED-SCOPE, prompt for a scope
+if not already set.  Mark up the output using `compilation-node`,
+replacing each file name with a small hyperlink, and display the
+result."
+  (with-current-buffer (go-guru--exec mode need-scope)
+    (go-guru--compilation-markup)))
+
+(defun go-guru--exec (mode &optional need-scope flags)
+  "Execute the Go guru in the specified MODE, passing it the
 selected region of the current buffer.  If NEED-SCOPE, prompt for
-a scope if not already set.  Process the output to replace each
-file name with a small hyperlink.  Display the result."
+a scope if not already set.  Return the output buffer."
   (if (not buffer-file-name)
       (error "Cannot use guru on a buffer without a file name"))
-  ;; It's not sufficient to save a modified buffer since if
-  ;; gofmt-before-save is on the before-save-hook, saving will
-  ;; disturb the selected region.
-  (if (buffer-modified-p)
-      (error "Please save the buffer before invoking go-guru"))
   (and need-scope
        (string-equal "" go-guru-scope)
        (go-guru-set-scope))
@@ -117,58 +121,110 @@ file name with a small hyperlink.  Display the result."
 			 (1- (position-bytes (point))))))
          (env-vars (go-root-and-paths))
          (goroot-env (concat "GOROOT=" (car env-vars)))
-         (gopath-env (concat "GOPATH=" (mapconcat #'identity (cdr env-vars) ":"))))
-    (with-current-buffer (get-buffer-create "*go-guru*")
+         (gopath-env (concat "GOPATH=" (mapconcat #'identity (cdr env-vars) ":")))
+	 (output-buffer (get-buffer-create "*go-guru*")))
+    (with-current-buffer output-buffer
+      (setq buffer-read-only nil)
+      (erase-buffer))
+    (with-current-buffer (get-buffer-create "*go-guru-input*")
       (setq buffer-read-only nil)
       (erase-buffer)
-      (insert "Go Guru\n")
-      (let ((args (list go-guru-command nil t nil
-			"-scope" go-guru-scope mode posn)))
-        ;; Log the command to *Messages*, for debugging.
-        (message "Command: %s:" args)
-        (message nil) ; clears/shrinks minibuffer
+      (go-guru--insert-modified-files)
+      (let* ((args (append (list "-modified"
+				 "-scope" go-guru-scope)
+			   flags
+			   (list mode posn))))
+	;; Log the command to *Messages*, for debugging.
+	(message "Command: %s:" args)
+	(message nil) ; clears/shrinks minibuffer
+	(message "Running guru %s..." mode)
+	;; Use dynamic binding to modify/restore the environment
+	(let* ((process-environment (list* goroot-env gopath-env process-environment))
+	       (c-p-args (append (list (point-min)
+				       (point-max)
+				       go-guru-command
+				       nil ; delete
+				       output-buffer
+				       t)
+				 args))
+	       (exitcode (apply #'call-process-region c-p-args)))
+	  ;; If the command fails, don't show the output buffer,
+	  ;; but use its contents (sans final \n) as an error.
+	  (unless (zerop exitcode)
+	    (with-current-buffer output-buffer
+	      (bury-buffer)
+	      (error "%s" (buffer-substring (point-min) (1- (point-max)))))))))
+    output-buffer))
 
-        (message "Running guru...")
-        ;; Use dynamic binding to modify/restore the environment
-        (let ((process-environment (list* goroot-env gopath-env process-environment)))
-            (apply #'call-process args)))
-      (insert "\n")
-      (compilation-mode)
-      (setq compilation-error-screen-columns nil)
+(defun go-guru--compilation-markup ()
+  "Present guru output in the current buffer using `compilation-mode'."
+  (insert "\n")
+  (compilation-mode)
+  (setq compilation-error-screen-columns nil)
+  
+  ;; Hide the file/line info to save space.
+  ;; Replace each with a little widget.
+  ;; compilation-mode + this loop = slooow.
+  ;; TODO(adonovan): have guru give us JSON
+  ;; and we'll do the markup directly.
+  (let ((buffer-read-only nil)
+	(p 1))
+    (while (not (null p))
+      (let ((np (compilation-next-single-property-change p 'compilation-message)))
+	(if np
+	    (when (equal (line-number-at-pos p) (line-number-at-pos np))
+	      ;; Using a fixed width greatly improves readability, so
+	      ;; if the filename is longer than 20, show ".../last/17chars.go".
+	      ;; This usually includes the last segment of the package name.
+	      ;; Don't show the line or column number.
+	      (let* ((loc (buffer-substring p np)) ; "/home/foo/go/pkg/file.go:1:2-3:4"
+		     (i (search ":" loc)))
+		(setq loc (cond
+			   ((null i)  "...")
+			   ((>= i 17) (concat "..." (substring loc (- i 17) i)))
+			   (t         (substring loc 0 i))))
+		;; np is (typically) the space following ":"; consume it too.
+		(put-text-property p np 'display (concat loc ":")))
+	      (goto-char np)
+	      (insert " ")
+	      (incf np))) ; so we don't get stuck (e.g. on a panic stack dump)
+	(setq p np)))
+    (message nil))
+  
+  (let ((w (display-buffer (current-buffer))))
+    (set-window-point w (point-min))))
 
-      ;; Hide the file/line info to save space.
-      ;; Replace each with a little widget.
-      ;; compilation-mode + this loop = slooow.
-      ;; TODO(adonovan): have guru give us JSON
-      ;; and we'll do the markup directly.
-      (let ((buffer-read-only nil)
-            (p 1))
-        (while (not (null p))
-          (let ((np (compilation-next-single-property-change p 'compilation-message)))
-            (if np
-                (when (equal (line-number-at-pos p) (line-number-at-pos np))
-                  ;; Using a fixed width greatly improves readability, so
-                  ;; if the filename is longer than 20, show ".../last/17chars.go".
-                  ;; This usually includes the last segment of the package name.
-                  ;; Don't show the line or column number.
-                  (let* ((loc (buffer-substring p np)) ; "/home/foo/go/pkg/file.go:1:2-3:4"
-                         (i (search ":" loc)))
-                    (setq loc (cond
-                               ((null i)  "...")
-                               ((>= i 17) (concat "..." (substring loc (- i 17) i)))
-                               (t         (substring loc 0 i))))
-                    ;; np is (typically) the space following ":"; consume it too.
-                    (put-text-property p np 'display (concat loc ":")))
-                  (goto-char np)
-                  (insert " ")
-                  (incf np))) ; so we don't get stuck (e.g. on a panic stack dump)
-            (setq p np)))
-        (message nil))
+(defun go-guru--insert-modified-files ()
+  "Insert the contents of each modified Go buffer into the
+current buffer in the format specified by guru's -modified flag."
+  (mapc #'(lambda (b)
+	    (and (buffer-modified-p b)
+		 (buffer-file-name b)
+		 (string= (file-name-extension (buffer-file-name b)) "go")
+		 (progn
+		   (insert (format "%s\n%d\n"
+				   (buffer-file-name b)
+                                   (go-guru--buffer-size-bytes b)))
+                   (insert-buffer-substring b))))
+	(buffer-list)))
 
-      (let ((w (display-buffer (current-buffer))))
-        (balance-windows)
-        (shrink-window-if-larger-than-buffer w)
-        (set-window-point w (point-min))))))
+(defun go-guru--buffer-size-bytes (&optional buffer)
+  "Return the number of bytes in the current buffer.
+If BUFFER, return the number of characters in that buffer instead."
+  (with-current-buffer (or buffer (current-buffer))
+    (string-bytes (buffer-substring (point-min)
+                                    (point-max)))))
+
+
+(defun go-guru--goto-pos (posn)
+  "Find the file containing the position POSN (of the form `file:line:col')
+set the point to it, switching the current buffer."
+  (let ((file-line-pos (split-string posn ":")))
+    (find-file (car file-line-pos))
+    (goto-char (point-min))
+    ;; NB: go/token's column offsets are byte- not rune-based.
+    (forward-line (1- (string-to-number (cadr file-line-pos))))
+    (forward-char (1- (string-to-number (caddr file-line-pos))))))
 
 ;;;###autoload
 (defun go-guru-callees ()
@@ -197,9 +253,16 @@ function containing the current point."
 
 ;;;###autoload
 (defun go-guru-definition ()
-  "Show the definition of the selected identifier."
+  "Jump to the definition of the selected identifier."
   (interactive)
-  (go-guru--run "definition"))
+  ;; TODO(adonovan): use -format=sexpr when available to avoid a
+  ;; dependency and to simplify parsing.
+  (let* ((res (with-current-buffer (go-guru--exec "definition" nil '("-format=json"))
+		(goto-char (point-min))
+		(cdr (car (json-read)))))
+	 (desc (cdr (assoc 'desc res))))
+    (go-guru--goto-pos (cdr (assoc 'objpos res)))
+    (message "%s" desc)))
 
 ;;;###autoload
 (defun go-guru-describe ()
